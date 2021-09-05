7a37301228e6de80fb19438d78bd1b28735993d3
Nguyen Viet Hung <hung.nv155790.hust@gmail.com>
2021-09-05 18:06:15 +0700

    Add card product

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,13 @@
 import React from "react";
-import png from "../dist/img/găng-tay-chơi-game.png"
+
+
 function Card(props) {
-    const {name, imgUrl, shopeeLink} = props;
+    const {name, imgUrl, linkShopee,price} = props;
+    
     console.group(props);
   return (
     <div
-      className="container product flex   flex-ai-c"
+      className="container product flex   flex-ai-c m-3"
       style={{maxWidth: "327px"}}
     >
       <figure
@@ -13,22 +15,22 @@ function Card(props) {
        className="image product__descript--img ml-3"
      >
        <img
-         style={{ borderRadius: "15px", height: "155px",
+         style={{ borderRadius: "15px", height: "155px", border: "1px solid #ededed",
          width: "155px"}}
-         src={png}
+         src={imgUrl}
          alt="FF-PUBG"
        />
      </figure>
       <div className="product__descript flex flex-ai-c flex-jc-sa flex-dr-c ml-2" style={{width:"50%",height:"100%"}}>
 
      <div className="product--text">
-       <p className="is-size-7 has-text-weight-medium mt-1 mb-1">
+       <p className="is-size-7 has-text-weight-medium mr-2 my-1">
          {name}
        </p>
-       <p className="is-size-7">50.000đ</p>
+       <p className="is-size-7">{price}</p>
      </div>
 
-     <button className="button is-small is-danger buy-btn">Mua ngay</button>
+     <a className="button is-small is-danger buy-btn" href={linkShopee} target="_blank">Mua ngay</a>
       </div>
     </div>
   );
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,8 +10,9 @@ function Cards() {
             {   
                 id: 'charge-1',
                 name: 'Bộ sạc dành cho Iphone 12 PD 20W Guxi C06 - Hàng chính hãng',
-                imgUrl: '',
-                link: ''
+                imgUrl: '../dist/img/earphone/tai_nghe_guxi_sp_n69.png',
+                link: '',
+                price: '120.000đ'
             }
         ]
     },
@@ -22,20 +23,23 @@ function Cards() {
             {   
                 id: 'earphone-1',
                 name: 'Tai nghe Guxi N67 - Hàng chính hãng',
-                imgUrl: '',
-                link: ''
+                imgUrl: 'https://cf.shopee.vn/file/6746623c2a2984518abe99fc7d6d1993',
+                link: 'https://shopee.vn/product/179381431/8871154244/',
+                price: '50.000đ'
             },
             {
                 id: 'earphone-2',
                 name: 'Tai nghe Guxi N68 - Hàng chính hãng',
-                imgUrl: '',
-                link: ''
+                imgUrl: 'https://cf.shopee.vn/file/3a9f37442f2619da57d4584723bcc292',
+                link: 'https://shopee.vn/product/179381431/9057879245/',
+                price: '50.000đ'
             },
             {
                 id: 'earphone-3',
                 name: 'Tai nghe Guxi N69 - Hàng chính hãng',
-                imgUrl: '',
-                link: ''
+                imgUrl: 'https://cf.shopee.vn/file/32c45e99184f69bdc8533f446bd2a0b5',
+                link: 'https://shopee.vn/product/179381431/2947390626/',
+                price: '50.000đ'
             }
         ]
     }
@@ -45,7 +49,9 @@ function Cards() {
       {productsStore!==null&& productsStore.map(store=>(
 
           store.products.map(product=>{
-              return <Card key={product.id} name={product.name} imgUrl ={product.imgUrl} linkShopee={product.link} />
+              return <Card key={product.id} name={product.name} imgUrl ={product.imgUrl} linkShopee={product.link} price = {
+                  product.price
+              } />
             })
             )
       )}
